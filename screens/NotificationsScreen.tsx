58ef4f9bd1cebd783cbbd865a37151201b493713
Kirill Tkachenko <tkachenko@quartsoft.com>
2022-08-19 13:09:15 +0300

    Adds id key to notifications

--- screens/NotificationsScreen.tsx
+++ screens/NotificationsScreen.tsx
@@ -6,11 +6,13 @@ import NotificationColumnList from "../components/notifications/NotificationColu
 export default function NotificationsScreen() {
     const notifications = [
         {
+            id: 1,
             imageUrl: 'https://icon-library.com/images/moderation-icon/moderation-icon-2.jpg',
             title: 'Рецепт промодеровано',
             text: 'Ваш рецепт "Медово гіричні джерки" успішно промодеровано, та доступний для перегляду.',
         },
         {
+            id: 2,
             imageUrl: 'http://cdn.onlinewebfonts.com/svg/img_369356.png',
             title: 'Оновлення додатку',
             text: 'Ми оновили додаток до версії 0.0.2. Перегляньте що нового у цьому оновленні.',
